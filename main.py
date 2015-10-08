d7c2cf3b2d10a59f12fc6d57e810e9ab29338d4a
tzaeru <tzaeru@gmail.com>
2015-10-08 21:39:45 +0300

    starting up

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+#!/usr/bin/python
+
 from tkinter import *
 from tkinter import ttk
+
+from threading import Thread
+import time
+
+import yaml
 
 def calculate(*args):
     try:
@@ -13,6 +20,9 @@ root.title("Feet to Meters")
 #root.geometry("170x200+30+30") 
 
 ttk.Style().configure("TButton", padding=6, relief="flat",
+   background="#595b59")
+
+ttk.Style().configure("TEntry", padding=6, relief="flat",
    background="#595b59")
 
 mainframe = ttk.Frame(root, padding="0 0 0 0")
@@ -31,11 +41,34 @@ background_label.grid(row=0,column=0, rowspan=6, columnspan=6)
 progress = ttk.Progressbar(mainframe)
 progress.grid(row=4,column=0)
 
-stacy = ttk.Button(mainframe, text = 'yoyo')
-stacy.grid(row=5,column=0)
+launch_button = ttk.Button(mainframe, text = 'Launch')
+launch_button.grid(row=5,column=0)
+
+nwn_path = StringVar()
+nwn_path_entry = ttk.Entry(mainframe, textvariable=nwn_path)
+nwn_path_entry.grid(row=5,column=2)
+nwn_path.set("NWN url")
+
+quit_button = ttk.Button(mainframe, text = "Quit")
+quit_button.grid(row = 5, column = 5)
 #stacy.place(x=0, y=0, relwidth=1, relheight=1)
 
 #background_label.grid_configure()
 #for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+test = 1
+
+def sleeper(i):
+    print ("thread %d sleeps for 5 seconds" % i)
+    global test
+    test = test + 1
+    time.sleep(5)
+    print ("thread %d woke up" % i)
+    test = test + 1
+    print(test)
+
+for i in range(10):
+    t = Thread(target=sleeper, args=(i,))
+    t.start()
 
 root.mainloop()
