8eba7cbd574ead70a131a8d686096eaee033551b
Jalmari Ikävalko <tzaeru@gmail.com>
2015-10-12 21:58:49 +0300

    GUI improvements

--- gui.py
+++ gui.py
@@ -4,8 +4,6 @@ from tkinter import ttk
 from threading import Thread
 import time
 
-import yaml
-
 import path_finder
 
 import os, subprocess
@@ -15,12 +13,56 @@ from tkinter.filedialog import askdirectory
 import dependency_manager
 
 from threading import Thread
-    
-root = Tk()
-root.title("Feet to Meters")
 
-ttk.Style().configure("TButton", padding=6, relief="flat",
-   background="#595b59")
+def _image_to_selected(event):
+    if event.widget.enabled == True:
+        event.widget["image"] = button_hovered_image
+
+def _image_to_deselected(event):
+    if event.widget.enabled == True:
+        event.widget["image"] = button_active_image
+
+def _image_to_disabled(widget):
+    widget.enabled = False
+    widget["image"] = button_disabled_image
+    widget["foreground"] = "#908080"
+
+def _image_to_enabled(widget):
+    widget.enabled = True
+    widget["image"] = button_active_image
+    widget["foreground"] = "#ffe0e0"
+
+def _create_label_button(text) -> Label:
+    label = Label(mainframe, text = text, compound="center",
+        image=button_active_image, borderwidth = 0,
+        relief = "flat", padx=0, pady=0,
+        font="TkHeadingFont 15 bold", foreground="#ffe0e0")
+
+    label.enabled = True
+
+    label.bind("<Enter>", _image_to_selected)
+    label.bind("<Leave>", _image_to_deselected)
+
+    return label
+
+def _create_label(text_v) -> Label:
+    label = Label(mainframe, textvariable = text_v,
+        borderwidth = 3, width=0,
+        relief = SUNKEN, padx=2, pady=2,
+        font="TkTextFond 10 bold", foreground="#efeeee",
+        background="#393b39")
+
+    return label
+
+root = Tk()
+root.title("NWN Launcher - Prisoners of The Mist")
+root.resizable(0,0)
+
+# Load the images we'll use
+background_image=PhotoImage(file="images/potm_title.png")
+button_active_image=PhotoImage(file="images/button_active.png")
+button_disabled_image=PhotoImage(file="images/button_disabled.png")
+button_hovered_image=PhotoImage(file="images/button_hovered.png")
 
 ttk.Style().configure("TEntry", padding=6, relief="flat",
    background="#595b59")
@@ -28,15 +70,15 @@ ttk.Style().configure("TEntry", padding=6, relief="flat",
 mainframe = ttk.Frame(root, padding="0 0 0 0")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-background_image=PhotoImage(file="images/potm_title.png")
 background_label = Label(mainframe, image=background_image, borderwidth=0)
 background_label.grid(row=0,column=0, rowspan=8, columnspan=8)
 
 progress = ttk.Progressbar(mainframe)
-#progress.grid(row=4,column=0)
+progress.grid(row=4,column=0)
 
-launch_button = ttk.Button(mainframe, text = 'Launch')
+launch_button = _create_label_button("Launch")
 launch_button.place(in_=mainframe, anchor="c", relx=.5, rely=.6)
+#launch_button.grid(row=6, column=0)
 
 def _trigger_launch(e):
     os.chdir(path_finder.get_path())
@@ -46,7 +88,7 @@ launch_button.bind("<Button-1>",_trigger_launch)
 
 nwn_path = StringVar()
 nwn_path.set(path_finder.get_path())
-nwn_path_label = ttk.Label(mainframe, textvariable=nwn_path, width=0)
+nwn_path_label = _create_label(nwn_path)
 nwn_path_label.place(in_=mainframe, anchor="c", relx=.5, rely=.7)
 
 def _trigger_path_dialogue(e):
@@ -59,16 +101,16 @@ nwn_path_label.bind("<Button-1>",_trigger_path_dialogue)
 def _trigger_quit(e):
     sys.exit()
 
-quit_button = ttk.Button(mainframe, text = "Quit")
+quit_button = _create_label_button("Quit")
 quit_button.grid(row = 7, column = 7)
 quit_button.bind("<Button-1>", _trigger_quit)
 
-update_button = ttk.Button(mainframe, text = "Update")
+update_button = _create_label_button("Update")
 update_button.grid(row = 7, column = 0)
-update_button["state"] = "disabled"
+_image_to_disabled(update_button)
 
 def _trigger_update(e):
-    update_button["state"] = "disabled"
+    _image_to_disabled(update_button)
     t = Thread(target=dependency_manager.do_update, args=())
     t.start()
 
@@ -82,7 +124,7 @@ update_status_label.grid(row = 7, column = 1)
 def _check_update_status():
     root.after(250, _check_update_status)
     if dependency_manager.status is dependency_manager.STATUS_UPDATES_AVAILABLE:
-        update_button["state"] = "normal"
+        _image_to_enabled(update_button)
 
     if dependency_manager.status is dependency_manager.STATUS_DOWNLOADING:
         update_status.set(dependency_manager.status + dependency_manager.file_being_downloaded)
@@ -90,18 +132,5 @@ def _check_update_status():
         update_status.set(dependency_manager.status)
 
 _check_update_status()
-
-"""def sleeper(i):
-    print ("thread %d sleeps for 5 seconds" % i)
-    global test
-    test = test + 1
-    time.sleep(5)
-    print ("thread %d woke up" % i)
-    test = test + 1
-    print(test)
-
-for i in range(10):
-    t = Thread(target=sleeper, args=(i,))
-    t.start()"""
 
 root.mainloop()
